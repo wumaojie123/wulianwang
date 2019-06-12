d3e75863144afa54b87576294b2b6beba37b6c9c
wumaojie123 <50513567+wumaojie123@users.noreply.github.com>
2019-06-12 14:36:35 +0800

    Create sketch_jun06a.ino

---
+++ sketch_jun06a/sketch_jun06a.ino
@@ -0,0 +1,25 @@
+const int TrigPin = 2; 
+const int EchoPin = 3; 
+float cm; 
+void setup() 
+{ 
+Serial.begin(9600); 
+pinMode(TrigPin, OUTPUT); 
+pinMode(EchoPin, INPUT); 
+pinMode(13,OUTPUT);
+} 
+void loop() 
+{ 
+digitalWrite(13, LOW);
+
+digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin 
+delayMicroseconds(2); 
+digitalWrite(TrigPin, HIGH); 
+delayMicroseconds(10); 
+digitalWrite(TrigPin, LOW); 
+
+cm = pulseIn(EchoPin, HIGH) / 58.0; //将回波时间换算成cm 
+cm = (int(cm * 100.0)) / 100.0; //保留两位小数 
+if (cm>=2 && cm<=10)
+digitalWrite(13, HIGH);
+}
