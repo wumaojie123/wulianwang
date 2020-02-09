42fdc774150df833b05ae3a21733d65490c8743d
wumaojie123 <50513567+wumaojie123@users.noreply.github.com>
2020-02-09 17:52:46 +0800

    test:webstrom

--- sketch_jun06a/index.vue
+++ sketch_jun06a/index.vue
@@ -4,9 +4,6 @@
   <cell v-if="isApprove" label="注销账号" value=" " arrow label-color="#333" @click.native="goAccountCancellation">
     <div slot="bottom" class="desc">操作后信息不可恢复，请谨慎考虑</div>
   </cell>
-  <div class="sd">666</div>
-  <div>555</div>
-  <div class="sd">888</div>
   <p>157623</p>
   <h3>ldfj</h3>
 </div>
